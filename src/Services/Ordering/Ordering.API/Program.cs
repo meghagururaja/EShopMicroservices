a6491c23934c1b7feea4536f82daf1510a77f476
megha <meghag@matrixcontrols.com>
2026-03-25 19:36:51 -0400

    Implemented RabitMQ MessaginG QUeue using MassTRansit Library for establishing async communication between Basket and ordering microservices. Also added YARPGateway project to route request to appropriate services

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the conatiner
 builder.Services
-    .AddApplicationServices()
+    .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices(builder.Configuration);
 
